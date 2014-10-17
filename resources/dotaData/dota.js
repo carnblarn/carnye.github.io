d83160bed4c5bac7eaa9dd168c8546587b5dbb9d
carnblarn <skcarney3@gmail.com>
2014-10-17 14:30:36 -0500

    dotadata

--- resources/dotaData/dota.js
+++ resources/dotaData/dota.js
@@ -1,8 +1,8 @@
 var app = angular.module('matches', ['ngTable']).
-controller('MainCtrl', function($scope, $http, $timeout, $resource, $filter, ngTableParams) {
+controller('MainCtrl', function($scope, $http, $filter, ngTableParams) {
 
     // var data = [{"matchId": 1, "teamOne": "TongFu", "people": "177", "items": "0", "winner": "TongFu", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "MUFC"}, {"matchId": 2, "teamOne": "iG", "people": "0", "items": "0", "winner": "iG", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "TongFu"}];
-    var Api = $resource('resources/dotaData/data.json');
+
 $scope.tableParams = new ngTableParams({
         page: 1,            // show first page
         count: 10,          // count per page
@@ -14,14 +14,15 @@ $scope.tableParams = new ngTableParams({
         getData: function($defer, params) {
             // use build-in angular filter
 
-           Api.get(params.url(), function(data) {
-                $timeout(function() {
-                    // update table params
-                    params.total(data.total);
-                    // set new data
-                    $defer.resolve(data.result);
-                }, 500);
-            });
+            $http.get('resources/dotaData/data.json').success(function(incData) {
+                console.log('Found Data');
+               data  = incData;
+               var orderedData = params.sorting() ?
+                                   $filter('orderBy')(data, params.orderBy()) :
+                                   data;
+
+               $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+             });
 
         }
     });
