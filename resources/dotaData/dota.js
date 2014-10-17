b90e6bd9adcddfd8982767994a9005ef14dcf871
carnblarn <skcarney3@gmail.com>
2014-10-17 14:00:10 -0500

    dotadata

--- resources/dotaData/dota.js
+++ resources/dotaData/dota.js
@@ -1,7 +1,8 @@
 var app = angular.module('matches', ['ui.grid']).
 controller('MainCtrl', function($scope, $http, $filter) {
 
-    $scope.matches = [{"matchId": 1, "teamOne": "TongFu", "people": "177", "items": "0", "winner": "TongFu", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "MUFC"}, {"matchId": 2, "teamOne": "iG", "people": "0", "items": "0", "winner": "iG", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "TongFu"}];
+    // $scope.matches = [{"matchId": 1, "teamOne": "TongFu", "people": "177", "items": "0", "winner": "TongFu", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "MUFC"}, {"matchId": 2, "teamOne": "iG", "people": "0", "items": "0", "winner": "iG", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "TongFu"}];
+    $scope.matches;
     $http.get('resources/dotaData/data.json').success(function(data) {
         console.log('Found Data');
        $scope.matches = data;
