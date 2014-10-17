8cc46f0c1264e2e66fcf718af27a5e2fa9128f7f
carnblarn <skcarney3@gmail.com>
2014-10-17 14:17:34 -0500

    dotadata

--- resources/dotaData/dota.js
+++ resources/dotaData/dota.js
@@ -1,11 +1,28 @@
-var app = angular.module('matches', ['ui.grid']).
-controller('MainCtrl', function($scope, $http, $filter) {
+var app = angular.module('matches', ['ngTable']).
+controller('MainCtrl', function($scope, $http, $filter, ngTableParams) {
 
-    // $scope.matches = [{"matchId": 1, "teamOne": "TongFu", "people": "177", "items": "0", "winner": "TongFu", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "MUFC"}, {"matchId": 2, "teamOne": "iG", "people": "0", "items": "0", "winner": "iG", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "TongFu"}];
-    $http.get('resources/dotaData/data.json').success(function(data) {
+    // var data = [{"matchId": 1, "teamOne": "TongFu", "people": "177", "items": "0", "winner": "TongFu", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "MUFC"}, {"matchId": 2, "teamOne": "iG", "people": "0", "items": "0", "winner": "iG", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "TongFu"}];
+    var data = [];
+    $http.get('resources/dotaData/data.json').success(function(incData) {
         console.log('Found Data');
-       $scope.matches = data;
+       data  = incData;
      });
+$scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10,          // count per page
+        sorting: {
+            matchId: 'desc'     // initial sorting
+        }
+    }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            // use build-in angular filter
+            var orderedData = params.sorting() ?
+                                $filter('orderBy')(data, params.orderBy()) :
+                                data;
 
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
 
 })
