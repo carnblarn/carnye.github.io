26c487eba6bf29c2a57d94f45eb75e8fe3f9d1f4
carnblarn <skcarney3@gmail.com>
2014-10-17 14:20:52 -0500

    dotadata

--- resources/dotaData/dota.js
+++ resources/dotaData/dota.js
@@ -3,10 +3,7 @@ controller('MainCtrl', function($scope, $http, $filter, ngTableParams) {
 
     // var data = [{"matchId": 1, "teamOne": "TongFu", "people": "177", "items": "0", "winner": "TongFu", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "MUFC"}, {"matchId": 2, "teamOne": "iG", "people": "0", "items": "0", "winner": "iG", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "TongFu"}];
     var data = [];
-    $http.get('resources/dotaData/data.json').success(function(incData) {
-        console.log('Found Data');
-       data  = incData;
-     });
+
 $scope.tableParams = new ngTableParams({
         page: 1,            // show first page
         count: 10,          // count per page
@@ -14,9 +11,14 @@ $scope.tableParams = new ngTableParams({
             matchId: 'desc'     // initial sorting
         }
     }, {
-        total: data.length, // length of data
+        total: 0, // length of data
         getData: function($defer, params) {
             // use build-in angular filter
+
+            $http.get('resources/dotaData/data.json').success(function(incData) {
+                console.log('Found Data');
+               data  = incData;
+             });
             var orderedData = params.sorting() ?
                                 $filter('orderBy')(data, params.orderBy()) :
                                 data;
