601e9c4937f24b54a45a35ecb4d6a9d688098dd8
carnblarn <skcarney3@gmail.com>
2014-10-17 17:17:30 -0500

    dotadata

--- resources/dotaData/dota.js
+++ resources/dotaData/dota.js
@@ -3,6 +3,9 @@ controller('MainCtrl', function($scope, $http, $filter, ngTableParams) {
 
     // var data = [{"matchId": 1, "teamOne": "TongFu", "people": "177", "items": "0", "winner": "TongFu", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "MUFC"}, {"matchId": 2, "teamOne": "iG", "people": "0", "items": "0", "winner": "iG", "teamTwoOdds": 0, "teamOneOdds": 0, "teamTwo": "TongFu"}];
 var query;
+$scope.$watch("filter.$", function(){
+    $scope.tableParams.reload();
+});
 $scope.tableParams = new ngTableParams({
         page: 1,            // show first page
         count: 10,          // count per page
@@ -13,16 +16,16 @@ $scope.tableParams = new ngTableParams({
         total: 3000, // length of data
         getData: function($defer, params) {
             // use build-in angular filter
-
-            $http.get('resources/dotaData/data.json').success(function(incData) {
+            if(!data){
+                $http.get('resources/dotaData/data.json').success(function(incData) {
                 console.log('Found Data');
                data  = incData;
+            }
+               var filteredData = $filter('filter')(data, $scope.filter);
                var orderedData = params.sorting() ?
-                                   $filter('orderBy')(data, params.orderBy()) :
-                                   data;
-                orderedData = params.filter() ?
-                                   $filter('filter')(data, query) :
-                                   data;
+                                   $filter('orderBy')(filteredData, params.orderBy()) :
+                                   filteredData;
+
                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
              });
 
