7660864b7f2334264092435b0046e66472707707
carnblarn <skcarney3@gmail.com>
2014-11-08 11:21:38 -0600

    resaerch

--- resources/userRank/controller.js
+++ resources/userRank/controller.js
@@ -3,60 +3,81 @@
         var app = angular.module('userRank', []);
 
         app.controller('MainCtrl', function($scope, $http, $window) {
-            var baseline = 1500;
+            var baseline = 1400;
             $scope.pieces = [{
                 image:'http://i.imgur.com/AWlAV9a.jpg',
                 title: 'Fall in Virginia',
-                finalScore: 2617,
+                finalScore: 4428,
                 index: 0
             },
-            {
-                image:'http://i.imgur.com/LQ4d15U.jpg',
-                title: 'Home library',
-                finalScore: 27,
+           {
+                image:'http://i.imgur.com/3j3Ravd.jpg',
+                title: 'Zoom burst firework',
+                finalScore: 10,
                 index: 1
             },
             {
                 image:'http://i.imgur.com/r2GeRYH.jpg',
                 title: 'So the Pink Ranger is busking in Toronto right now',
-                finalScore: 27,
+                finalScore: 3812,
                 index: 2
             },
             {
                 image:'http://i.imgur.com/6T8gGG1.jpg',
                 title: 'Fall at the great wall',
-                finalScore: 27,
+                finalScore: 799,
                 index: 3
             },
             {
                 image:'http://i.imgur.com/UcWHOZQ.jpg',
                 title: 'Sugar, sitting for her portrait',
-                finalScore: 27,
+                finalScore: 368,
                 index: 4
             },
-            {
-                image:'http://i.imgur.com/xHwml3j.jpg',
-                title: 'Nothing special, just thought my street looked nice tday',
-                finalScore: 27,
+             {
+                image:'http://i.imgur.com/daHiSHM.jpg',
+                title: 'My favourite photo from my trip to Afria earlier this year',
+                finalScore: 34,
                 index: 5
             },
             {
-                image:'http://max.distractify.com/wp-content/uploads//2014/06//lichtenstein-castle-ryan-wyckoff.jpg',
-                title: 'Lichtenstein Castle in Germany',
-                finalScore: 27,
+                image:'http://i.imgur.com/5xVcwMx.jpg',
+                title: 'This is a picture of the sun. Note the magnetic field lines',
+                finalScore: 1239,
                 index: 6
             },
             {
-                image:'http://i.imgur.com/daHiSHM.jpg',
-                title: 'My favourite photo from my trip to Afria earlier this year',
-                finalScore: 27,
+                image:'http://i.imgur.com/Fs07ubx.jpg',
+                title: 'Kasteel de Haar, Netherlands',
+                finalScore: 1969,
                 index: 7
             },
             {
-                image:'http://i.imgur.com/3j3Ravd.jpg',
-                title: 'Zoom burst firework',
-                finalScore: 27,
+                image:'http://i.imgur.com/q4WB9PY.jpg',
+                title: 'who disturbs my slumber?!',
+                finalScore: 682,
                 index: 8
+            },
+            {
+                image:'http://i.imgur.com/zQZ6F8I.jpg',
+                title: 'Just Norway, being awesome',
+                finalScore: 805,
+                index: 9
+            },
+
+            {
+                image:'http://max.distractify.com/wp-content/uploads//2014/06//lichtenstein-castle-ryan-wyckoff.jpg',
+                title: 'Lichtenstein Castle in Germany',
+                finalScore: 22,
+                index: 10
+            },
+
+
+             {
+                image:'http://i.imgur.com/LQ4d15U.jpg',
+                title: 'Home library',
+                finalScore: 4130,
+                index: 11
             },
 
             ];
@@ -64,7 +85,7 @@
                 if(stance == 0){
                     return;
                 }
-                $scope.weight += stance * ($scope.pieces[index].finalScore - baseline) * .4/baseline;
+                $scope.weight += stance * ($scope.pieces[index].finalScore - baseline) * .3/baseline;
                 if($scope.weight <= .1){
                     $scope.weight = .1;
                 }
