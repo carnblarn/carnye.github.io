61e138643b3eb143717c3aa86695211309202666
carnblarn <skcarney3@gmail.com>
2014-11-16 23:42:50 -0600

    new project

--- resources/instagram.js
+++ resources/instagram.js
@@ -1,4 +1,78 @@
 /*! jQuery Instagram - v0.3.1 - 2014-06-19
 * http://potomak.github.com/jquery-instagram
 * Copyright (c) 2014 Giovanni Cappellotto; Licensed MIT */
-!function(a){function b(b){var c="https://api.instagram.com/v1",d={};if(null==b.accessToken&&null==b.clientId)throw"You must provide an access token or a client id";if(d=a.extend(d,{access_token:b.accessToken||"",client_id:b.clientId||"",count:b.count||""}),null!=b.url)c=b.url;else if(null!=b.hash)c+="/tags/"+b.hash+"/media/recent";else if(null!=b.search)c+="/media/search",d=a.extend(d,b.search);else if(null!=b.userId){if(null==b.accessToken)throw"You must provide an access token";c+="/users/"+b.userId+"/media/recent"}else null!=b.location?(c+="/locations/"+b.location.id+"/media/recent",delete b.location.id,d=a.extend(d,b.location)):c+="/media/popular";return{url:c,data:d}}a.fn.instagram=function(c){var d=this;c=a.extend({},a.fn.instagram.defaults,c);var e=b(c);return a.ajax({dataType:"jsonp",url:e.url,data:e.data,success:function(a){d.trigger("didLoadInstagram",a)}}),this.trigger("willLoadInstagram",c),this},a.fn.instagram.defaults={accessToken:null,clientId:null,count:null,url:null,hash:null,userId:null,location:null,search:null}}(jQuery);
+(function($) {
+
+  function composeRequest(options) {
+    var url = 'https://api.instagram.com/v1';
+    var data = {};
+
+    if (options.accessToken == null && options.clientId == null) {
+      throw 'You must provide an access token or a client id';
+    }
+
+    data = $.extend(data, {
+      access_token: options.accessToken || '',
+      client_id: options.clientId || '',
+      count: options.count || ''
+    });
+
+    if (options.url != null) {
+      url = options.url;
+    }
+    else if (options.hash != null) {
+      url += '/tags/' + options.hash + '/media/recent';
+    }
+    else if (options.search != null) {
+      url += '/media/search';
+      data = $.extend(data, options.search);
+    }
+    else if (options.userId != null) {
+      if (options.accessToken == null) {
+        throw 'You must provide an access token';
+      }
+      url += '/users/' + options.userId + '/media/recent';
+    }
+    else if (options.location != null) {
+      url += '/locations/' + options.location.id + '/media/recent';
+      delete options.location.id;
+      data = $.extend(data, options.location);
+    }
+    else {
+      url += '/media/popular';
+    }
+
+    return {url: url, data: data};
+  }
+
+  $.fn.instagram = function(options) {
+    var that = this;
+    options = $.extend({}, $.fn.instagram.defaults, options);
+    var request = composeRequest(options);
+
+    $.ajax({
+      dataType: "jsonp",
+      url: request.url,
+      data: request.data,
+      success: function(response) {
+        that.trigger('didLoadInstagram', response);
+      }
+    });
+
+    this.trigger('willLoadInstagram', options);
+
+    return this;
+  };
+
+  $.fn.instagram.defaults = {
+    accessToken: null,
+    clientId: null,
+    count: null,
+    url: null,
+    hash: null,
+    userId: null,
+    location: null,
+    search: null
+  };
+
+}(jQuery));
