4329f9528275ecdd7f633952d333d6f4912d67b1
carnblarn <skcarney3@gmail.com>
2014-11-30 14:37:45 -0600

    hello

--- resources/spotify/spotify.js
+++ resources/spotify/spotify.js
@@ -6,7 +6,7 @@ controller('MainCtrl', function($scope, $http) {
   $scope.customizedTracks;
   $scope.$watch('age', function(){
     if(($scope.age !== undefined) && ($scope.gender !== undefined)){
-      $http.get('customTracks' + ($scope.age + $scope.gender) + '.json').
+      $http.get('resource/spotify/customTracks' + ($scope.age + $scope.gender) + '.json').
       success(function(data, status, headers, config) {
         $scope.customTrackList = data;
         console.log(data);
