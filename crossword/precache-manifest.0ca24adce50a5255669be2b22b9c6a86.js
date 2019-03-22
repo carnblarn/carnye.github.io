778e6ce9519fd98b63e6a36538969cd7c43a31f6
carney <skcarney3@gmail.com>
2019-03-22 10:50:46 -0400

    crossword

---
+++ crossword/precache-manifest.0ca24adce50a5255669be2b22b9c6a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e06261f0ae2e001eb0d2",
+    "url": "/crossword/static/js/runtime~main.59db1c93.js"
+  },
+  {
+    "revision": "2b83f3effc2d878a2a1d",
+    "url": "/crossword/static/js/main.e891239e.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/crossword/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "2b83f3effc2d878a2a1d",
+    "url": "/crossword/static/css/main.b15743ed.chunk.css"
+  },
+  {
+    "revision": "a68d62d6f16f134599010c7b64e937e9",
+    "url": "/crossword/index.html"
+  }
+];
