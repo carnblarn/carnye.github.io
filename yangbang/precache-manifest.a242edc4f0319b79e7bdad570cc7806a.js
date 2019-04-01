126026c585411b468b185216168c61a364e613a9
carney <skcarney3@gmail.com>
2019-03-31 21:37:36 -0400

    Adding in a yangbang prototype

---
+++ yangbang/precache-manifest.a242edc4f0319b79e7bdad570cc7806a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b741026ff7a0d4d1e737",
+    "url": "/yangbang/static/js/runtime~main.349d9f8f.js"
+  },
+  {
+    "revision": "c823324c0881099882e6",
+    "url": "/yangbang/static/js/main.635d6834.chunk.js"
+  },
+  {
+    "revision": "bbefe6dac5c3d3d58934",
+    "url": "/yangbang/static/js/2.9450d529.chunk.js"
+  },
+  {
+    "revision": "c823324c0881099882e6",
+    "url": "/yangbang/static/css/main.7bbc6997.chunk.css"
+  },
+  {
+    "revision": "67df5e63690409d2ead64492d632df57",
+    "url": "/yangbang/index.html"
+  }
+];
