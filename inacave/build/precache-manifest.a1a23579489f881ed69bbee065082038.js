d6604a3a8cb5bd2679c4189dc762fc43f58dbbe7
carney <skcarney3@gmail.com>
2019-04-08 20:26:18 -0400

    making it easier

---
+++ inacave/build/precache-manifest.a1a23579489f881ed69bbee065082038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe3df33fe0708a189c8",
+    "url": "/inacave/static/js/runtime~main.f0f5ccd7.js"
+  },
+  {
+    "revision": "c38b1bbd019680263e0e",
+    "url": "/inacave/static/js/main.ac7e4c44.chunk.js"
+  },
+  {
+    "revision": "8e74d15af96d0dacc417",
+    "url": "/inacave/static/js/2.ae7c55ce.chunk.js"
+  },
+  {
+    "revision": "c38b1bbd019680263e0e",
+    "url": "/inacave/static/css/main.f671efa9.chunk.css"
+  },
+  {
+    "revision": "771210db50d5a2ac90c9fc766d9352bf",
+    "url": "/inacave/index.html"
+  }
+];
