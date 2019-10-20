f164c2b73fe65d083461e2f59d3dd7ce3daf67e3
carney <skcarney3@gmail.com>
2019-10-20 14:31:11 -0400

    updates

---
+++ statue/build/precache-manifest.f32da51175baee23ba7031f4dfe7de89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44552f65ebf5c13c43ea233981988813",
+    "url": "/crossword/index.html"
+  },
+  {
+    "revision": "90c95c3ad0eb5d39be8a",
+    "url": "/crossword/static/css/main.2c1861d0.chunk.css"
+  },
+  {
+    "revision": "4b2aee1e9d138dfd1e6c",
+    "url": "/crossword/static/js/2.61e3ffbc.chunk.js"
+  },
+  {
+    "revision": "90c95c3ad0eb5d39be8a",
+    "url": "/crossword/static/js/main.5cacbcd7.chunk.js"
+  },
+  {
+    "revision": "0d22be13de7b85fd59a6",
+    "url": "/crossword/static/js/runtime-main.6c940714.js"
+  }
+]);
