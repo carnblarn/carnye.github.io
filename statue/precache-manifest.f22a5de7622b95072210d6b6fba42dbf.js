fdc9b50693550c572531451d936a075202ec8011
carney <skcarney3@gmail.com>
2019-10-20 14:23:42 -0400

    adding the statue

---
+++ statue/precache-manifest.f22a5de7622b95072210d6b6fba42dbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125a33d06df69be5b360d50c493e7737",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8da03f5a87cb1fc2a2c",
+    "url": "/static/css/main.2c1861d0.chunk.css"
+  },
+  {
+    "revision": "4b2aee1e9d138dfd1e6c",
+    "url": "/static/js/2.61e3ffbc.chunk.js"
+  },
+  {
+    "revision": "e8da03f5a87cb1fc2a2c",
+    "url": "/static/js/main.068f4f1b.chunk.js"
+  },
+  {
+    "revision": "421be721b4b89c098596",
+    "url": "/static/js/runtime-main.c1f65636.js"
+  }
+]);
