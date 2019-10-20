3eab8cf748398a7ebea98ca12bc1fd4f0c944339
carney <skcarney3@gmail.com>
2019-10-20 15:01:01 -0400

    plz

---
+++ statue/build/precache-manifest.688365d08aff6eb3512ecc280c7c32ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23f79a14794b9927dbbebfa40cbd86f",
+    "url": "/statue/index.html"
+  },
+  {
+    "revision": "b566dde338aea128f0e1",
+    "url": "/statue/static/css/main.2c1861d0.chunk.css"
+  },
+  {
+    "revision": "4b2aee1e9d138dfd1e6c",
+    "url": "/statue/static/js/2.61e3ffbc.chunk.js"
+  },
+  {
+    "revision": "b566dde338aea128f0e1",
+    "url": "/statue/static/js/main.7e63db98.chunk.js"
+  },
+  {
+    "revision": "e34ee7a029f56130af65",
+    "url": "/statue/static/js/runtime-main.78d3fcd3.js"
+  }
+]);
