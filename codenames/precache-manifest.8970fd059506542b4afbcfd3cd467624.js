98424e3f351af27767d7774c351c498513325fbf
carney <skcarney3@gmail.com>
2019-12-21 12:59:27 -0500

    adding in codenames

---
+++ codenames/precache-manifest.8970fd059506542b4afbcfd3cd467624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71cb88db0a5daa8f5cc5eaed1eee8d56",
+    "url": "/codenames/index.html"
+  },
+  {
+    "revision": "ac6f11b95174d24d7229",
+    "url": "/codenames/static/css/main.901e12ea.chunk.css"
+  },
+  {
+    "revision": "b7fc7f94287239c17358",
+    "url": "/codenames/static/js/2.9b83e5c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/codenames/static/js/2.9b83e5c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac6f11b95174d24d7229",
+    "url": "/codenames/static/js/main.57af4e47.chunk.js"
+  },
+  {
+    "revision": "5f32c6ddfa5b41464632",
+    "url": "/codenames/static/js/runtime-main.0c4c8f66.js"
+  }
+]);
