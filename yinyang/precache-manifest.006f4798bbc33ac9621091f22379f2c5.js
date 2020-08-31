52e7e1822a15343425cf0d351aebcfb271b55931
carney <skcarney3@gmail.com>
2020-08-30 20:11:12 -0400

    Adding a new game

---
+++ yinyang/precache-manifest.006f4798bbc33ac9621091f22379f2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ef30f54756b646cdc8f84d3e798354",
+    "url": "/yinyang/index.html"
+  },
+  {
+    "revision": "eb34c7c17db2c81e1c69",
+    "url": "/yinyang/static/css/main.df33b227.chunk.css"
+  },
+  {
+    "revision": "40b50497ebefc1922a88",
+    "url": "/yinyang/static/js/2.be4b5680.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/yinyang/static/js/2.be4b5680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb34c7c17db2c81e1c69",
+    "url": "/yinyang/static/js/main.b2a9182e.chunk.js"
+  },
+  {
+    "revision": "3b6451d7695f617abe31",
+    "url": "/yinyang/static/js/runtime-main.cc4ab8b6.js"
+  }
+]);
