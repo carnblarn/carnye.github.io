5ff247f79d2aab14f95bff33e55c35a698ceff9f
carney <skcarney3@gmail.com>
2020-09-29 13:07:40 -0400

    removing unsused applications

---
+++ crossword/build/precache-manifest.352eb5e4e7f8b3aed80ed388e4d76624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e06261f0ae2e001eb0d2",
+    "url": "/crossword/static/js/runtime~main.59db1c93.js"
+  },
+  {
+    "revision": "dcd7b6be6f83445aa78d",
+    "url": "/crossword/static/js/main.292187d3.chunk.js"
+  },
+  {
+    "revision": "34b9a668fe8a527ef860",
+    "url": "/crossword/static/js/2.9a4476dc.chunk.js"
+  },
+  {
+    "revision": "dcd7b6be6f83445aa78d",
+    "url": "/crossword/static/css/main.b15743ed.chunk.css"
+  },
+  {
+    "revision": "6715d62d4900364b96687b7cc43f9e43",
+    "url": "/crossword/index.html"
+  }
+];
---
+++ crossword/build/precache-manifest.b3e927889759ff9c9bca181ea586532c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e06261f0ae2e001eb0d2",
+    "url": "/crossword/static/js/runtime~main.59db1c93.js"
+  },
+  {
+    "revision": "c08b6f5f5bc3f0fc08d5",
+    "url": "/crossword/static/js/main.147c81a5.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/crossword/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "c08b6f5f5bc3f0fc08d5",
+    "url": "/crossword/static/css/main.b15743ed.chunk.css"
+  },
+  {
+    "revision": "20c8b92fa43d0327edb0930a31197f6a",
+    "url": "/crossword/index.html"
+  }
+];
