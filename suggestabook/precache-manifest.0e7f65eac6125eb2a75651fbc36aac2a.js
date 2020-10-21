ce7d9760e54bed38afa7671a4ef59b8e8df1be9b
carney <skcarney3@gmail.com>
2020-10-21 10:19:02 -0400

    adding in suggested books

---
+++ suggestabook/precache-manifest.0e7f65eac6125eb2a75651fbc36aac2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1510aa384fa062735165ef3d32e4e9a",
+    "url": "/suggestabook/index.html"
+  },
+  {
+    "revision": "89233b98bd928fdaa788",
+    "url": "/suggestabook/static/css/main.c9b96610.chunk.css"
+  },
+  {
+    "revision": "eb6a80ac375680c4242e",
+    "url": "/suggestabook/static/js/2.5e9fd48f.chunk.js"
+  },
+  {
+    "revision": "c1dc937edb42d8b04bf53bdd1005a494",
+    "url": "/suggestabook/static/js/2.5e9fd48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89233b98bd928fdaa788",
+    "url": "/suggestabook/static/js/main.ae08a238.chunk.js"
+  },
+  {
+    "revision": "0ac8f86d9894817cc2d6",
+    "url": "/suggestabook/static/js/runtime-main.70accec8.js"
+  }
+]);
